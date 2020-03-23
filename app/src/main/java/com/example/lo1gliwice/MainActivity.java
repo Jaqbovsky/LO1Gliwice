1f3c09dcbc2032cefb4a1500239c3744a6245284
Jaqbovsky <47501201+Jaqbovsky@users.noreply.github.com>
2020-03-23 21:58:12 +0100

    version 0.1 ------------------ changelog: -support for actionBar -support for drawer Activty -sideBar menu

--- app/src/main/java/com/example/lo1gliwice/MainActivity.java
+++ app/src/main/java/com/example/lo1gliwice/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.lo1gliwice;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.navigationView);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawerOpen, R.string.drawerClose);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.menu_mainPage:
+                Toast.makeText(MainActivity.this, "Strona główna", Toast.LENGTH_SHORT).show();
+            break;
+
+            case R.id.menu_clasroomChange:
+                Toast.makeText(MainActivity.this, "Zamiana klas", Toast.LENGTH_SHORT).show();
+            break;
+
+            case R.id.menu_setting:
+                Toast.makeText(MainActivity.this, "Ustawienia", Toast.LENGTH_SHORT).show();
+            break;
+
+            case R.id.menu_information:
+                Toast.makeText(MainActivity.this, "Informacje", Toast.LENGTH_SHORT).show();
+            break;
+        }
+
+        return false;
     }
 }
